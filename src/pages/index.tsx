17dfeac7cccdf48d89cee5b36fd305ab307f5a0c
Mauricio Farinelli <mauricio@sweetiq.com>
2018-03-20 22:55:35 -0400

    using getInitialProps and getting wordpress json from its API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/layout';
 
-export default () => (
-  <Layout>
-    <div>Hello World.</div>
-  </Layout>
-) 
+interface AppProps {
+    pageJson: object
+}
+
+class Home extends React.Component<any, AppProps> {
+    static async getInitialProps(): Promise<object> {
+        const url: string = 'http://159.89.116.112/adriana/wp-json/wp/v2/pages/2';
+        const page: Promise<object> = await fetch(url);
+        const pageJson: object = await page.json();
+        return { pageJson };
+    }
+
+    render() {
+        const { pageJson } = this.props;
+        const content: string = pageJson.content.rendered;
+
+        return (
+          <Layout>
+            <div>
+              <h1>Hello World.</h1>
+              <div dangerouslySetInnerHTML={{__html: content }} />
+            </div>
+          </Layout>
+        );
+    }
+}
+
+export default Home;
